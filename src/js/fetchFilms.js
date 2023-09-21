18a85917fe936a39fc497a20e96e0def1419361e
mattmalicki <mateusz.malicki96@gmail.com>
2023-09-21 16:06:29 +0200

    adding api key and creating featch method

---
+++ src/js/fetchFilms.js
@@ -0,0 +1,22 @@
+const API_KEY =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmEwYTgwNDZiYWRmMDlmOGM2MWVhOWMwNjFkMjc1ZCIsInN1YiI6IjY1MGM0MDg4NDRlYTU0MDBjNjMxZDVjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QCvrUYw290qbZ5ir3M1mVaFysI8g2yCPJXwVdcerhR4';
+const API_URL = 'https://api.themoviedb.org/3/';
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+export async function featchFilms({ requestType = 'trending/all/day', requestParams = '' }) {
+  try {
+    const params = new URLSearchParams({ ...requestParams });
+    const response = await fetch(`${API_URL}${requestType}?${params}`, options);
+    const films = await response.json();
+    return films;
+  } catch (err) {
+    return err;
+  }
+}
