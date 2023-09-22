7e7a19163d38151b8596523fe2e86739118c40e0
mattmalicki <mateusz.malicki96@gmail.com>
2023-09-22 07:17:46 +0200

    backing index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
 import './sass/main.scss';
-import { fetchAll } from './js/fetchAll';
-import { getTVGenres, getMoviesGenres } from './js/getGenres';
-fetchAll();
-const genres = getMoviesGenres();
-console.log(genres);
