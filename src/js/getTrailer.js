5cd1e6093034d890080e35ee63f1b893c0c5c24b
mattmalicki <mateusz.malicki96@gmail.com>
2023-09-22 22:31:44 +0200

    made function for videos

--- src/js/getTrailer.js
+++ src/js/getTrailer.js
@@ -1,17 +1,19 @@
-import { fetchFromApi } from "./fetchFromApi";
+import { fetchFromApi } from './fetchFromApi';
 
 export async function getTrailer(type, id) {
-    const requestPath = `${type}/${id}/videos`
-    try {
-        const trailers = await fetchFromApi({ requestPath });
-        console.log(trailers);
-        if (!trailers.id || !trailers.results) {
-            return;
-        }
-        for (trailer of trailers.results) {
-            if (trailer.site === 'YouTube') {
-                return `https://www.youtube.com/watch?v=${trailer.key}`;
-            }
-        }
+  const requestPath = `${type}/${id}/videos`;
+  try {
+    const trailers = await fetchFromApi({ requestPath });
+    console.log(trailers);
+    if (!trailers.id || !trailers.results) {
+      return;
     }
+    for (trailer of trailers.results) {
+      if (trailer.site === 'YouTube') {
+        return `https://www.youtube.com/watch?v=${trailer.key}`;
+      }
+    }
+  } catch (err) {
+    return err;
+  }
 }
