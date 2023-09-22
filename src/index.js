1c8a2277d1449653af65c3b5ea0ab8feaad973b0
mattmalicki <mateusz.malicki96@gmail.com>
2023-09-22 18:02:45 +0200

    adding searchAll function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+import { searchAllBy } from './js/searchBy';
+
+async function test() {
+  try {
+    const gimme = await searchAllBy('dates', 1);
+    console.log(gimme);
+  } catch (err) {
+    console.log(`Error occured: ${err.toString()}`);
+  }
+}
+test();
