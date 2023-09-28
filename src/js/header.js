4e8e0a6aaacd55071df1c00f2bf48a7efc10b80e
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2023-09-28 19:37:44 +0200

    added card and theme

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,13 @@
+import { loadStorage, saveStorage } from './localStorage';
+
 const chk = document.getElementById('chk');
 
-chk.addEventListener('change', themeDark);
+chk.addEventListener('change', changeTheme);
 
-function themeDark() {
-  document.body.classList.toggle('dark');
+function changeTheme() {
+  //sam sprawdzi czy jest dark czy nie i albo usunie albo doda
+  document.querySelector('body').classList.toggle('dark');
+
+  //pobiera dane z local i sprawdza ktore jest i zapisuje przeciwne do tego co jest.
+  loadStorage('theme') === 'dark' ? saveStorage('theme', 'light') : saveStorage('theme', 'dark');
 }
-const settings = {
-  theme: 'dark',
-  isAuthenticated: true,
-};
-
-localStorage.setItem('settings', JSON.stringify(settings));
