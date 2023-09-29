8f5e870c4807e3e04988b51c2e15026880d33219
AnnaSosik <125738831+AnnaSosik@users.noreply.github.com>
2023-09-29 23:36:28 +0200

    pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,61 +1,39 @@
-const TRENDING_URL = 'https://api.themoviedb.org/3/trending/movie/day';
-const { paginationContainer } = getRefs();
+// import { f.ex popularMovies} from'./'//
+import Pagination from 'tui-pagination';
 
+const options = {
+  totalItems: 1000,
+  itemsPerPage: 20,
+  visiblePages: 5,
+  page: 1,
+  centerAlign: true,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' + '<span>⋅⋅⋅</span>' + '</a>',
+  },
+};
 
-export async function fetchTrendMovies(page) {
-    try {
-      const { data } = await axios.get(
-        `${TRENDING_URL_URL}?api_key=${API_KEY}&page=${page}`
-      );
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-export const paginationSettings = {
-    startPage: 1,
-    searchType: null,
-    pagination: null,
-    totalItemsHome: null,
-}
-export const initPagination = ({ page, itemsPerPage, totalItems }) => {
-    const options = {
-        totalItems,
-        itemsPerPage,
-        page,
-        visiblePages: 5,
-        centerAlign: true,
-        template: {
-            page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-            currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-            moveButton:
-                '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</a>',
-            disabledMoveButton:
-                '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                '</span>',
-            moreButton:
-                '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                '<span class="tui-ico-ellip">...</span>' +
-                '</a>'
-        }
-    }
-
-    
 const pagination = new Pagination('pagination', options);
-pagination.on('afterMove', async ({ page }) => {
-    if (paginationSettings.searchType === 'main') {
-        try {
-            const { results } = await fetchTrendMovies(page);
-            //in this place function about render gallery//
-            window.scrollTo(pageYOffset,0);
-        } catch (error) {
-            console.log(error);
-        }
-        else if { paginationSettings.searchType === 'search') {
-            try {
 
-        }
+pagination.on('afterMove', function (eventData) {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  const currentPage = eventData.page;
+  const startIndex = (currentPage - 1) * options.itemsPerPage;
+  const endIndex = startIndex + options.itemsPerPage;
+
+  if (currentPage > 1) {
+    getPopularMoviesData(startIndex, endIndex);
+  }
+});
+
+// export default pagination//;
