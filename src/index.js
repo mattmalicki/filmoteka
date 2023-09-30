793377c97580ba9965d851947acc15e9d5be9d68
Krzysztof Kryczka <83279465+krzysztof-kryczka@users.noreply.github.com>
2023-09-30 09:03:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import './sass/main.scss';
 import './js/header.js';
 import './js/fetchFromApi.js';
 import './js/renderFilms.js';
-import './js/firebase/firebase.js';
+//import './js/firebase/firebase.js';
+import './js/firebase/modal-auth.js';
+import './js/firebase/userAuth.js';
