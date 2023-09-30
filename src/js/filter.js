055461114d5defacb762e02c0aba6c246975f027
mattmalicki <mateusz.malicki96@gmail.com>
2023-09-30 10:02:35 +0200

    checking

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,23 +1,37 @@
 import { getAllGenres } from './getGenres';
-import '../sass/partials/_filter.scss';
+
+const testBttn = document.querySelector('#test-filter');
 
 async function createChck() {
   const element = document.querySelector('.filter__genres');
   const genresArray = await getAllGenres();
-  console.log(genresArray);
   genresArray.forEach(genre => {
     const genreEl = document.createElement('input');
     const label = document.createElement('label');
     genreEl.value = genre.id;
     genreEl.id = genre.id;
     genreEl.type = 'checkbox';
+    genreEl.dataset.idName = genre.name;
     genreEl.name = 'genres';
-    genreEl.classList.add('filter__genres-item');
+    genreEl.classList.add('filter__genres-check');
     label.for = 'genres';
     label.classList.add('filter__genres-item');
     label.textContent = genre.name;
-    element.append(genreEl, label);
+    label.prepend(genreEl);
+    element.append(label);
   });
 }
 
 createChck();
+
+testBttn.addEventListener('click', () => {
+  const genres = document.querySelectorAll('[name="genres"]');
+  const types = document.querySelectorAll('[name="media-type"]');
+  const type = [];
+  genres.forEach(el => {
+    el.checked ? type.push(el.id) : null;
+  });
+  console.log(type);
+});
+
+function filterChecked() {}
