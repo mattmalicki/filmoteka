fe3604b4cf63592f6864c2b8c7d274fffd59727b
krzysztof-kryczka <83279465+krzysztof-kryczka@users.noreply.github.com>
2023-10-02 17:01:32 +0000

    Deploying to gh-pages from @ mattmalicki/filmoteka@df5fdbb51b656edd8959669a7b94a04bd14c0815 🚀

---
+++ index.ace17f6d.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",(function(){let e=document.querySelector(".header"),t=document.querySelector(".js-header__sticky");e.getBoundingClientRect().bottom<0?t.style.display="block":t.style.display="none"}));
+//# sourceMappingURL=index.ace17f6d.js.map
