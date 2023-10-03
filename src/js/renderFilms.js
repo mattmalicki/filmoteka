5039d7ef1bd6beca82b595a1cfa16393ac6dac4e
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-03 19:11:45 +0200

    final touches

--- src/js/renderFilms.js
+++ src/js/renderFilms.js
@@ -3,13 +3,13 @@ import { createCard } from './filmCard';
 import { getFilter } from './filter';
 import Notiflix from 'notiflix';
 import './modalFilm';
+import pagination from './pagination';
+import { loaderToggle } from './loader';
 
 const header = document.querySelector('.header-library');
 const data = {
-  apiPage: 1,
-  apiAll: 0,
   page: 1,
-  all: 0,
+  allPages: 0,
   keyname: '',
   filters: [],
 };
@@ -17,40 +17,35 @@ const formEl = document.querySelector('#search-form');
 const listEl = document.querySelector('.films__grid');
 const inputEl = document.querySelector('[name="searchQuery"]');
 if (!header) {
-  formEl.addEventListener('submit', showFilms);
+  formEl.addEventListener('submit', onSubmit);
 }
-async function showFilms(event) {
+
+function onSubmit(event) {
   event.preventDefault();
-  clearList();
+  data.page = 1;
   data.filters = getFilter();
   data.keyname = inputEl.value;
+  showFilms();
+}
+
+function showFilms() {
+  clearList();
   if (data.filters.length > 0) {
     if (data.keyname === '') {
-      const movies = await api.fetchMovieOnlyGenres(data.filters, data.page);
-      const arrayEl = await createCard(movies.results);
-      !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
-      listEl.append(...arrayEl);
+      genreNoName();
     } else {
-      const movies = await api.fetchMovieOnlyGenres(data.filters, data.page);
-      const arrayEl = await createCard(movies.results);
-      !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
-      listEl.append(...arrayEl);
+      genreWithName();
     }
   } else {
     if (data.keyname === '') {
-      const movies = await api.fetchTrendingMovies(data.page);
-      console.log(movies);
-      const arrayEl = await createCard(movies.results);
-      !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
-      listEl.append(...arrayEl);
+      noNameNoGenre();
     } else {
-      const movies = await api.fetchMoviesWithName(data.keyname, data.page);
-      const arrayEl = await createCard(movies.results);
-      !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
-      listEl.append(...arrayEl);
+      nameNoGenre();
     }
   }
 }
+
+showFilms();
 
 function clearList() {
   while (listEl.firstChild) {
@@ -58,4 +53,58 @@ function clearList() {
   }
 }
 
-function fillData() {}
+async function genreNoName() {
+  try {
+    loaderToggle();
+    const movies = await api.fetchMovieOnlyGenres(data.filters, data.page);
+    const arrayEl = await createCard(movies.results);
+    data.allPages = movies.total_pages;
+    data.page++;
+    !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
+    listEl.append(...arrayEl);
+  } catch (err) {
+    console.log(`Error: ${err.toString()}`);
+  } finally {
+    loaderToggle();
+  }
+}
+
+function genreWithName() {
+  new Notiflix.Notify.failure("Can't search movies by name and genres.");
+}
+
+async function nameNoGenre() {
+  try {
+    loaderToggle();
+    const movies = await api.fetchMoviesWithName(data.keyname, data.page);
+    const arrayEl = await createCard(sortArray(movies.results));
+    data.allPages = movies.total_pages;
+    data.page++;
+    !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
+    listEl.append(...arrayEl);
+  } catch (err) {
+    console.log(`Error: ${err.toString()}`);
+  } finally {
+    loaderToggle();
+  }
+}
+
+async function noNameNoGenre() {
+  try {
+    loaderToggle();
+    const movies = await api.fetchTrendingMovies(data.page);
+    const arrayEl = await createCard(movies.results);
+    data.allPages = movies.total_pages;
+    data.page++;
+    !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
+    listEl.append(...arrayEl);
+  } catch (err) {
+    console.log(`Error: ${err.toString()}`);
+  } finally {
+    loaderToggle();
+  }
+}
+
+function sortArray(array) {
+  return array.sort((firsObj, secObj) => secObj.popularity - firsObj.popularity);
+}
