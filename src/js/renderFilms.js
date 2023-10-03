27d94d4e008c3bed72fa6cc29962045ab75c0d0d
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-03 15:55:27 +0200

    e

--- src/js/renderFilms.js
+++ src/js/renderFilms.js
@@ -5,10 +5,17 @@ import Notiflix from 'notiflix';
 import './modalFilm';
 
 const header = document.querySelector('.header-library');
+const data = {
+  apiPage: 1,
+  apiAll: 0,
+  page: 1,
+  all: 0,
+  keyname: '',
+  filters: [],
+};
 const formEl = document.querySelector('#search-form');
 const listEl = document.querySelector('.films__grid');
 const inputEl = document.querySelector('[name="searchQuery"]');
-let page = 1;
 if (!header) {
   formEl.addEventListener('submit', showFilms);
 }
@@ -17,22 +24,22 @@ async function showFilms(event) {
   while (listEl.firstChild) {
     listEl.firstChild.remove();
   }
-  const filters = getFilter();
-  const keyname = inputEl.value;
-  if (filters.length > 0) {
-    if (keyname === '') {
-      const movies = await api.fetchMovieOnlyGenres(filters, page);
+  data.filters = getFilter();
+  data.keyname = inputEl.value;
+  if (data.filters.length > 0) {
+    if (data.keyname === '') {
+      const movies = await api.fetchMovieOnlyGenres(data.filters, data.page);
       const arrayEl = await createCard(movies.results);
       !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
       listEl.append(...arrayEl);
     } else {
-      const movies = await api.fetchMovieOnlyGenres(filters, page);
+      const movies = await api.fetchMovieOnlyGenres(data.filters, data.page);
       const arrayEl = await createCard(movies.results);
       !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
       listEl.append(...arrayEl);
     }
   } else {
-    const movies = await api.fetchTrendingMovies(page);
+    const movies = await api.fetchTrendingMovies(data.page);
     const arrayEl = await createCard(movies.results);
     !arrayEl.length ? new Notiflix.Notify.failure('No movies found') : null;
     listEl.append(...arrayEl);
