89b4f560384c269c5da004b4b6d8d783d7b7d8b7
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-04 16:13:30 +0200

    modal team closing clicking outside

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -6,7 +6,13 @@ function closeModalTeam() {
   modalTeam.classList.add('is-hidden'); // Dodaj klasę is-hidden, aby ukryć modal
 }
 closeModalTeamButton.addEventListener('click', closeModalTeam);
+modalTeam.addEventListener('click', closeModalTeamOutside);
 
+function closeModalTeamOutside(event) {
+  if (event.target.className.includes('team-backdrop')) {
+    modalTeam.classList.toggle('is-hidden');
+  }
+}
 backdrop.addEventListener('click', event => {
   if (event.target === backdropTeam) {
     closeModalTeam();
