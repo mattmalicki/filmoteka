307f174a4015ce81ade8bbe63b7627b295011359
mattmalicki <127140342+mattmalicki@users.noreply.github.com>
2023-10-10 13:35:56 +0000

    Deploying to gh-pages from @ mattmalicki/filmoteka@e810fab1a28e00596e8258d5ad89db8468bf9aa1 🚀

---
+++ index.224ebebf.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#footer__button"),t=document.querySelector(".footer__button-icon"),o=document.querySelector("#footer");e.addEventListener("click",(function(){o.classList.toggle("hide"),t.classList.toggle("flip")}));
+//# sourceMappingURL=index.224ebebf.js.map
